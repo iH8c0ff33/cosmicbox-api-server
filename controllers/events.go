5ef7d3bc0194a65f1dfae039758b10715c2ada00
Daniele Monteleone <daniele@monteleone.ml>
2017-10-29 20:59:25 +0100

    event: Add methods

--- controllers/events.go
+++ controllers/events.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"git.deutron.ml/iH8c0ff33/cosmicbox-api-server/models"
 	"github.com/gin-gonic/gin"
 )
@@ -8,13 +10,23 @@ import (
 // Events is the controller for /event path
 func Events(g *gin.RouterGroup) {
 	g.GET("", getAllEvents)
-	g.POST("/:id", getEventFromID)
+	g.GET("/:id", getEventFromID)
+	g.POST("/:id", createEventFromID)
 }
 
 func getAllEvents(c *gin.Context) {
-	c.JSON(200, models.GetEvents())
+	c.JSON(http.StatusOK, models.GetEvents())
 }
 
 func getEventFromID(c *gin.Context) {
-	c.JSON(200, models.CreateEvent(c.Param("id")))
+	event, err := models.GetEvent(c.Param("id"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, err)
+		return
+	}
+	c.JSON(http.StatusOK, event)
+}
+
+func createEventFromID(c *gin.Context) {
+	c.JSON(http.StatusOK, models.CreateEvent(c.Param("id")))
 }
--- models/event.go
+++ models/event.go
@@ -1,11 +1,23 @@
 package models
 
+import "errors"
+
 type Event struct {
 	ID string `json:"id"`
 }
 
 func GetEvents() []Event {
 	return db.events
+}
+
+func GetEvent(id string) (Event, error) {
+	for _, event := range db.events {
+		if event.ID == id {
+			return event, nil
+		}
+	}
+	
+	return Event{}, errors.New("e_not_found")
 }
 
 func CreateEvent(id string) Event {
