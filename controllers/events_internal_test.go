ad47ffb7f60138bb251383d68cb812d720192eec
Daniele Monteleone <daniele@monteleone.ml>
2017-10-29 18:01:57 +0100

    tests: Use utils in events test

--- controllers/events_internal_test.go
+++ controllers/events_internal_test.go
@@ -2,32 +2,13 @@ package controllers
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/gin-gonic/gin"
 	"git.deutron.ml/iH8c0ff33/cosmicbox-api-server/models"
+	"git.deutron.ml/iH8c0ff33/cosmicbox-api-server/utils"
 )
 
 func Test_getAllEvents(t *testing.T) {
-	req, err := http.NewRequest("GET", "/events", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := gin.New()
-	handler.GET("/events", getAllEvents)
-
-	handler.ServeHTTP(rr, req)
-
 	models.Initialize()
-
-	if rr.Code != http.StatusOK {
-		t.Fatalf("code: %d != %d", rr.Code, http.StatusOK)
-	}
-
-	if rr.Body.String() != "[]" {
-		t.Fatalf("body: %s != %s", rr.Body.String(), "[]")
-	}
+	utils.CheckGetHandlerResp(getAllEvents, http.StatusOK, "[]", t)
 }
